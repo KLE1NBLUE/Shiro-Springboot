cabe7847c32e3827870f1a32c30d5a5c0baa576e
kle1nblue <1049834396@qq.com>
2021-05-16 15:35:17 +0800

    配置

--- src/main/java/com/zane/realm/AccountRealm.java
+++ src/main/java/com/zane/realm/AccountRealm.java
@@ -2,11 +2,17 @@ package com.zane.realm;
 
 import com.zane.entity.Account;
 import com.zane.service.AccountService;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class AccountRealm extends AuthorizingRealm {
 
@@ -16,7 +22,18 @@ public class AccountRealm extends AuthorizingRealm {
     //授权
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        return null;
+        //获取当前用户登陆信息
+        Subject subject = SecurityUtils.getSubject();
+        Account account = (Account) subject.getPrincipal();
+
+        //设置角色
+        Set<String> roles = new HashSet<>();
+        roles.add(account.getRole());
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo(roles);
+
+        //设置权限
+        info.addStringPermission(account.getPerms());
+        return info;
     }
 
     //认证
